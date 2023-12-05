46dba3d844dd13519922ea4a0fc95e48a3a0f8cd
Kassiani <131450663+kmota01@users.noreply.github.com>
2023-12-05 22:33:18 +0100

    Delete manta_run.sh

--- manta_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bash/bin
-
-source ./config.sh
-
-#step 1: Define your variables(work_dir & sample) and create your workflow
-python ./configManta.py --normalBam=/home/kmota01/test_align/ref_map/${sample}_sorted.bam --referenceFasta=/home/kmota01/h38/hg38.fa --runDir=$work_dir/sv_call/manta
-
-#step 2: Run manta workflow
-python $work_dir/sv_call/manta/runWorkflow.py
