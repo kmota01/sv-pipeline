b948fd109f623c1517c7ef12e7100ec58e1c7106
Kassiani <131450663+kmota01@users.noreply.github.com>
2023-12-06 11:56:26 +0100

    Add choice for Lumpy & Manta

--- 123456_pipeline.sh
+++ 123456_pipeline.sh
@@ -2,6 +2,7 @@
 
 source ./config.sh
 
+#For case sensitivity (turn argument into lowercase)
 software=${software,,}
 
 script1=./1_qc_and_trimming.sh
@@ -11,6 +12,7 @@ script4=./4_set_insert_size.sh
 script5=./5_lumpy_run.sh
 script6=./6_manta_run.sh
 
+#Lumpy and Manta both need a coordinate-sorted alignment file (.bam) as an input. If this input exists the preprossecing of the sequencing-data will not be repeated.
 if [[ ! -e $work_dir/ref_map/${sample}.bam ]]; then
 	for proc in $script1 $script2;do
         	bash $proc -i $sample -w $work_dir
